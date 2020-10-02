4c9d056ad3d3c84f85f48a91764477bbf3e966ec
Poablin <kristoffer.elvejord@gmail.com>
2020-10-02 12:05:28 +0200

    up

---
+++ Intro til M3/Converting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    class Converting
+    {
+        static void Something()
+        {
+            // to string
+            var x = true;
+            double d = 99.123;
+            string s = "" + x;
+            //to int
+            int i = Convert.ToInt32(d);
+        }
+    }
+}
--- Intro til M3/Program.cs
+++ Intro til M3/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Intro_til_M3
+namespace Intro
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Deklarasjon
+            bool b;
+            int i;
+            float f;
+            double d;
+            string s;
+            // Tilordning
+            b = true;
+            i = 5;
+            f = 3.2f;
+            d = 1.2;
+            s = "Hei";
+            // Deklarasjon & Tilordning
+            bool b2 = false;
+            var b3 = true;
+
+            Console.WriteLine(f);
+
+            
+            
+            
+            
+            //if (args.Length == 0)
+            //{
+            //    Console.WriteLine("Hallo! Ingen argumenter");
+            //}
+            //else 
+            //{ 
+            //    Console.WriteLine("Hallo! Hvor mange argumenter: " + args.Length);
+            //}
+            
+            //foreach (var text in args)
+            //{
+            //    Console.WriteLine("Parameter: " + text);
+            //}
         }
     }
 }
