bbf438564138684e5822f0430fc263216bb53f46
Poablin <kristoffer.elvejord@gmail.com>
2020-10-02 12:50:15 +0200

    Update Program.cs

--- Intro til M3/Program.cs
+++ Intro til M3/Program.cs
@@ -6,41 +6,29 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-            // Deklarasjon
-            bool b;
-            int i;
-            float f;
-            double d;
-            string s;
-            // Tilordning
-            b = true;
-            i = 5;
-            f = 3.2f;
-            d = 1.2;
-            s = "Hei";
-            // Deklarasjon & Tilordning
-            bool b2 = false;
-            var b3 = true;
-
-            Console.WriteLine(f);
-
-            
-            
-            
-            
-            //if (args.Length == 0)
-            //{
-            //    Console.WriteLine("Hallo! Ingen argumenter");
-            //}
-            //else 
-            //{ 
-            //    Console.WriteLine("Hallo! Hvor mange argumenter: " + args.Length);
-            //}
-            
-            //foreach (var text in args)
-            //{
-            //    Console.WriteLine("Parameter: " + text);
-            //}
+            var range = 250;
+            var counts = new int[range];
+            int total = 0;
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine().ToLower() ?? string.Empty; 
+                foreach (var character in text)
+                {
+                    total++;
+                    counts[(int)character]++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        var percent = 100 * (double)counts[i] / total;
+                        string output = character + " - " + percent.ToString("F2") + "%";
+                        Console.WriteLine(output);
+                    }
+                }
+            }
         }
     }
 }
