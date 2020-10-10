2e9fb0634eda29916966c0bf1df4647fb9b0036b
Poablin <kristoffer.elvejord@gmail.com>
2020-10-10 21:24:21 +0200

    Update Program.cs

--- Intro til M3/Program.cs
+++ Intro til M3/Program.cs
@@ -6,29 +6,43 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-            var range = 250;
+            ReadTextAndShowCharCounts(250);
+        }
+
+        private static void ReadTextAndShowCharCounts(int range)
+        {
             var counts = new int[range];
             int total = 0;
             string text = "something";
             while (!string.IsNullOrWhiteSpace(text))
             {
-                text = Console.ReadLine().ToLower() ?? string.Empty; 
-                foreach (var character in text)
-                {
-                    total++;
-                    counts[(int)character]++;
-                }
-                for (var i = 0; i < range; i++)
-                {
-                    if (counts[i] > 0)
-                    {
-                        var character = (char)i;
-                        var percent = 100 * (double)counts[i] / total;
-                        string output = character + " - " + percent.ToString("F2") + "%";
-                        Console.WriteLine(output);
-                    }
-                }
+                text = Console.ReadLine().ToLower();
+                total = CountChars(text, total, counts);
+                ShowCounts(range, counts, total);
             }
+        }
+
+        private static void ShowCounts(int range, int[] counts, int total)
+        {
+            for (var i = 0; i < range; i++)
+            {
+                if (counts[i] <= 0) continue;
+                var character = (char) i;
+                var percent = 100 * (double) counts[i] / total;
+                string output = character + " - " + percent.ToString("F2") + "%";
+                Console.WriteLine(output);
+            }
+        }
+
+        private static int CountChars(string text, int total, int[] counts)
+        {
+            foreach (var character in text)
+            {
+                total++;
+                counts[(int) character]++;
+            }
+
+            return total;
         }
     }
 }
