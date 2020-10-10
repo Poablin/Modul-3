33bd2b98205a040bee466a15d5226c979316ade4
Poablin <kristoffer.elvejord@gmail.com>
2020-10-10 21:35:43 +0200

    Start

---
+++ PassordGenerator/PassordGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                string output =
+                    @"PasswordGenerator  
+  Options:
+  - l = lower case letter
+  - L = upper case letter
+  - d = digit
+  - s = special character (!""#¤%&/(){}[]
+Example: PasswordGenerator 14 lLssdd
+         Min. 1 lower case
+         Min. 1 upper case
+         Min. 2 special characters
+         Min. 2 digits";
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
