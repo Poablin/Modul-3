e8a86236b10be536fcdb08e59aefd74daec10ea3
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-12 13:42:27 +0200

    Update Program.cs

--- OrdGaater/OrdGaater/Program.cs
+++ OrdGaater/OrdGaater/Program.cs
@@ -11,10 +11,18 @@ namespace Testeapp
         {
             var path = @"C:\Users\GET\source\repos\Modul-3\OrdGaater\OrdGaater\ordliste.txt";
             string[] allLines = File.ReadAllLines(path);
+            MakeListFromWords(allLines);
+        }
+
+        private static void MakeListFromWords(string[] allLines)
+        {
+            var lastWord = string.Empty;
             foreach (var line in allLines)
             {
                 var parts = line.Split("\t");
-                Console.WriteLine(parts[1]);
+                var word = parts[1];
+                if (word!=lastWord) Console.WriteLine(word);
+                lastWord = word;
             }
         }
     }
