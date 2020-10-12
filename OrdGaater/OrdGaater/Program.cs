b3a35d5c7e3dc4204aaff15bc36728f08569ee03
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-12 13:16:51 +0200

    Update

---
+++ OrdGaater/OrdGaater/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Testeapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\GET\source\repos\Testeapp\Testeapp\ordliste.txt";
+            string[] allLines = File.ReadAllLines(path);
+            foreach (var line in allLines)
+            {
+                var parts = line.Split("\t");
+                Console.WriteLine(parts[1]);
+            }
+        }
+    }
+}
