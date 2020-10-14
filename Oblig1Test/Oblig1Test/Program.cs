5f9f879f63681fac87eb26046eec9cd7b4a9ca72
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-14 14:50:50 +0200

    Update

---
+++ Oblig1Test/Oblig1Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Oblig1Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person
+            {
+                Id = 17,
+                FirstName = "Ola",
+                LastName = "Nordmann",
+                BirthYear = 2000,
+                DeathYear = 3000,
+                Father = new Person() {Id = 23, FirstName = "Per"},
+                Mother = new Person() {Id = 29, FirstName = "Lise"},
+            };
+
+            Console.WriteLine(p.GetDescription());
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public Person Father { get; set; }
+        public Person Mother { get; set; }
+
+        public string GetDescription()
+        {
+            return $"{FirstName} {LastName} (Id={Id}) Født: {BirthYear} Død: {DeathYear} Far: {Father.FirstName} (Id={Father.Id}) Mor: {Mother.FirstName} (Id={Mother.Id})";
+        }
+    }
+}
---
+++ Oblig1Test/ObligTester/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using Oblig1Test;
+
+namespace ObligTester
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestAllFields()
+        {
+            var p = new Person
+            {
+                Id = 17,
+                FirstName = "Ola",
+                LastName = "Nordmann",
+                BirthYear = 2000,
+                DeathYear = 3000,
+                Father = new Person() { Id = 23, FirstName = "Per" },
+                Mother = new Person() { Id = 29, FirstName = "Lise" },
+            };
+
+            var actualDescription = p.GetDescription();
+            var expectedDescription = "Ola Nordmann (Id=17) Født: 2000 Død: 3000 Far: Per (Id=23) Mor: Lise (Id=29)";
+
+            Assert.AreEqual(expectedDescription, actualDescription);
+        }
+    }
+}
