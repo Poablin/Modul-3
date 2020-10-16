797d63ab87ac6ec6393cbfbe0b9eb6b342b91402
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-16 09:12:35 +0200

    start

---
+++ TrePaaRad/TrePaaRad/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TrePaaRad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = "   a b c\r\n ┌─────┐\r\n1│o    │\r\n2│    o│\r\n3│× ×  │\r\n └─────┘";
+            Console.WriteLine(board);
+        }
+    }
+}
