b6f1371c0319391dc07767cc44d502e78a9ebf9a
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-19 09:28:15 +0200

    Update BoardView.cs

--- TrePaaRad/TrePaaRad/BoardView.cs
+++ TrePaaRad/TrePaaRad/BoardView.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Console = System.Console;
 
 namespace TrePaaRad
 {
@@ -8,12 +10,29 @@ namespace TrePaaRad
         {
 
             var board = boardModel.BoxContents;
-            Console.WriteLine("  a b c");
-            Console.WriteLine(" ┌─────┐");
-            Console.WriteLine("1│o    │");
-            Console.WriteLine("2│    o│");
-            Console.WriteLine("3│× ×  │");
-            Console.WriteLine(" └─────┘");
+            Console.WriteLine("   a b c");
+            Console.WriteLine(" ┌───────┐");
+            var startIndex = 0;
+            ShowOneLine(0, board);
+            ShowOneLine(3, board);
+            ShowOneLine(6, board);
+            Console.WriteLine(" └───────┘");
+
+
+
+        }
+        public static void ShowOneLine(int startIndex, CellContent[] board)
+        {
+            var lineNo = 1 + startIndex / 3;
+            Console.Write(lineNo +  "│");
+            for (var i = startIndex; i < startIndex + 3; i++)
+            {
+                if (i > startIndex) Console.Write(' ');
+                var isBlank = board[i] == CellContent.None;
+                var isCross = board[i] == CellContent.Cross;
+                Console.Write(isBlank ? ' ' : isCross ? 'x' : 'o');
+            }
+            Console.WriteLine("│");
         }
     }
 }
