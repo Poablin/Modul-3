0ba893608c8da2989917097119e8a2c03e91144d
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-19 13:47:18 +0200

    uppydate

--- RandomShapes/RandomBoxes/Program.cs
+++ RandomShapes/RandomBoxes/Program.cs
@@ -27,7 +27,7 @@ namespace RandomBoxes
         {
             var random = new Random();
             var shapes = new Shape[5];
-            shapes[0] = new Text(10, 5, "Hurra!", random);
+            shapes[0] = new Text(10, 5, "Teksten funkææær", random);
             for (var i = 1; i < shapes.Length; i++)
             {
                 if (random.Next(0, 2) == 0)
--- RandomShapes/RandomBoxes/Text.cs
+++ RandomShapes/RandomBoxes/Text.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RandomBoxes
 {
     class Text : Shape
     {
-        private string _text;
+        private readonly string _text;
         public Text(int x, int y, string text, Random random) : base(random)
         {
             Y = y;
