8ce48ec6876b98a16ab96bcb0fbca4bf7a81abc3
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-19 13:12:02 +0200

    Update Program.cs

--- RandomShapes/RandomBoxes/Program.cs
+++ RandomShapes/RandomBoxes/Program.cs
@@ -28,7 +28,7 @@ namespace RandomBoxes
             var random = new Random();
             var shapes = new Shape[5];
             shapes[0] = new Text(10, 5, "Hurra!", random);
-            for (var i = 0; i < shapes.Length; i++)
+            for (var i = 1; i < shapes.Length; i++)
             {
                 if (random.Next(0, 2) == 0)
                     shapes[i] = new Rectangle(random, _width, _height);
