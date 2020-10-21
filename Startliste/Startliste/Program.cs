3d5275cb844559b208c799ef453600cf284edb14
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-21 10:28:38 +0200

    uppy

---
+++ Startliste/Startliste/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Startliste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Registration>();
+
+            using (var file = new StreamReader("startlist.csv"))
+            {
+                string header = file.ReadLine();
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
