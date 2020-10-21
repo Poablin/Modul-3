55d2ba560be15ed11ed4a5fa6974bd5177277917
Poablin <kristoffer.elvejord@gmail.com>
2020-10-21 18:55:26 +0200

    Legger til Registration objektet i liste

--- Startliste/Startliste/Program.cs
+++ Startliste/Startliste/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Startliste
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Startliste
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var list = new List<Registration>(); 
+            using (var stream = new StreamReader("startlist.csv"))
+            {
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    var str = line;
+                    var stringArray = str.Split(',');
+                    var registration = new Registration(stringArray[0], stringArray[1], stringArray[2], stringArray[3], stringArray[4], stringArray[5]);
+                    list.Add(registration);
+                }
+
+                foreach (var content in list)
+                {
+                    Console.WriteLine(content.Name);
+                }
+            }
         }
     }
 }
---
+++ Startliste/Startliste/Registration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Startliste
+{
+    class Registration
+    { 
+        public Registration(string startNumber, string name, string club, string nationality, string @group, string @class)
+        {
+            StartNumber = startNumber;
+            Name = name;
+            Club = club;
+            Nationality = nationality;
+            Group = @group;
+            Class = @class;
+        }
+
+        public string StartNumber { get; private set; }
+        public string Name { get; private set; }
+        public string Club { get; private set; }
+        public string Nationality { get; private set; }
+        public string Group { get; private set; }
+        public string @Class { get; private set; }
+    }
+}
