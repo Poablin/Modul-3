7cba939d156c225116a589f0f80c7c65a405ffec
Poablin <65161335+Poablin@users.noreply.github.com>
2020-11-03 14:36:18 +0100

    Update

---
+++ AsyncAndAwait/AsyncAndAwait/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var downloadTask = DownloadLineFromDatabase();
+            var lineWriteTask = WriteNewLine();
+            var downloadedLine = await downloadTask;
+            var newWrittenLine = await lineWriteTask;
+            Console.WriteLine("Finished");
+        }
+
+        private static async Task<string> DownloadLineFromDatabase()
+        {
+            Console.WriteLine("Downloading from database...");
+            await Task.Delay(3000);
+            return "Downloaded";
+        }
+        private static async Task<string> WriteNewLine()
+        {
+            Console.WriteLine("Writing new file...");
+            await Task.Delay(2000);
+            return "Written Line";
+        }
+    }
+}
