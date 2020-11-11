8283e4819fa7d3add6341f57ed726cce8d41bc76
Poablin <kristoffer.elvejord@gmail.com>
2020-11-11 12:07:08 +0100

    Update

---
+++ DictionaryClassTesting/DictionaryClassTesting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryClassTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> testDictionary = new Dictionary<string, string>();
+            testDictionary.Add("tst", "Heijegerentestdictionary");
+            testDictionary.Add("jada", "tssssssssssssssssssssssssssssssst");
+            Console.WriteLine(testDictionary["tst"]);
+            Console.WriteLine(testDictionary["jada"]);
+
+            // Tester KeyNotFoundException
+            try
+            {
+                Console.WriteLine(testDictionary["yes"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("KeyNotFoundException fordi yes keyen ikke finnes");
+            }
+
+        }
+    }
+}
